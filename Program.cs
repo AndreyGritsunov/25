9214bfd8fcd752ad77dbb299b012a027e6af3873
unknown <gritsunov85@gmail.com>
2022-08-23 07:22:36 +0600

    end

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int[] arr = new int[2];
+for(int i = 0; i < 2; i++)
+{
+    Console.WriteLine("Введите число " + Convert.ToString(i + 1) + ": ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+
+    if(i == 1) {
+        int index = 0;
+        int sum = 1;
+        while(index < arr[i]){
+            sum = sum * arr[0];
+            index++;
+        }
+        Console.Write(sum);
+    } 
+}   
